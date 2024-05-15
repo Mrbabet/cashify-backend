20b5dd2edffcf3a0209cd2c0e6a69894397108b3
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2024-05-15 10:19:45 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,7 +68,7 @@ const login = async (req, res) => {
     return res.status(200).send({
       accessToken,
       refreshToken,
-      userData: {
+      user: {
         email,
         balance: user.balance,
         id: user._id,
